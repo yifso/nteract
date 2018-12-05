f927cd1686acb0c184b7c46d6cb865e808a3d313
Shelby Sturgis <ssturgis@netflix.com>
2018-12-04 22:43:57 -0800

    adding ContentRef to Nav to pass down to NotebookMenu

--- applications/jupyter-extension/nteract_on_jupyter/app/components/nav.js
+++ applications/jupyter-extension/nteract_on_jupyter/app/components/nav.js
@@ -1,6 +1,7 @@
 /* @flow strict */
 import * as React from "react";
 import { NotebookMenu } from "@nteract/connected-components";
+import type { ContentRef } from "@nteract/core";
 
 type NavSectionProps = {
   children: React.Node
@@ -35,7 +36,8 @@ export class NavSection extends React.Component<NavSectionProps, null> {
 }
 
 type NavProps = {
-  children: React.ChildrenArray<React.Element<*>>
+  children: React.ChildrenArray<React.Element<*>>,
+  contentRef: ContentRef
 };
 
 export class Nav extends React.Component<NavProps, null> {
