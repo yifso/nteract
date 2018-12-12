c347ec0ae823b1e791e517d582d25348edc1a823
Safia Abdalla <safia@safia.rocks>
2018-12-12 10:02:52 -0600

    Update next config to support TypeScript

--- applications/play/next.config.js
+++ applications/play/next.config.js
@@ -1,5 +1,4 @@
 const configurator = require("@nteract/webpack-configurator");
+const withTypescript = require("@zeit/next-typescript");
 
-module.exports = {
-  webpack: configurator.nextWebpack
-};
+module.exports = withTypescript({ webpack: configurator.nextWebpack });
