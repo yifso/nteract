1a70bf31f115c64b0902289e788e938d40da6057
Safia Abdalla <safia@safia.rocks>
2018-12-16 13:52:57 -0800

    Use styled-components in consoles.tsx

--- applications/play/components/consoles.tsx
+++ applications/play/components/consoles.tsx
@@ -1,4 +1,10 @@
 import React, { ChangeEvent, FormEvent } from "react";
+import styled from "styled-components";
+
+const Image = styled.img`
+  vertical-align: middle;
+  margin: 0 0 7px 0px;
+`;
 
 class BinderLogo extends React.Component {
   render() {
@@ -10,18 +16,12 @@ class BinderLogo extends React.Component {
           target="_blank"
           rel="noreferrer noopener"
         >
-          <img
+          <Image
             src="https://mybinder.org/static/logo.svg?v=f9f0d927b67cc9dc99d788c822ca21c0"
             alt="binder logo"
             height="20px"
           />
         </a>
-        <style jsx>{`
-          img {
-            vertical-align: middle;
-            margin: 0 0 7px 0px;
-          }
-        `}</style>
       </React.Fragment>
     );
   }
@@ -35,6 +35,18 @@ type BinderTextInputProps = {
   name: string;
 };
 
+const Label = styled.label`
+  & input {
+    font-family: inherit;
+    font-size: inherit;
+  }
+
+  & span {
+    width: 12em;
+    display: inline-block;
+  }
+`;
+
 class BinderTextInput extends React.Component<BinderTextInputProps> {
   render() {
     const { onChange, value, id, labelText, name } = this.props;
@@ -42,7 +54,7 @@ class BinderTextInput extends React.Component<BinderTextInputProps> {
     return (
       <React.Fragment>
         <fieldset className="binder">
-          <label htmlFor={id}>
+          <Label htmlFor={id}>
             <span>{labelText}</span>
             <input
               id={id}
@@ -52,19 +64,8 @@ class BinderTextInput extends React.Component<BinderTextInputProps> {
               value={value}
               size={80}
             />
-          </label>
+          </Label>
         </fieldset>
-        <style jsx>{`
-          input {
-            font-family: inherit;
-            font-size: inherit;
-          }
-
-          label span {
-            width: 12em;
-            display: inline-block;
-          }
-        `}</style>
       </React.Fragment>
     );
   }
@@ -74,6 +75,31 @@ type BinderFormProps = {
   onSubmit(event: FormEvent<HTMLFormElement>): void;
 };
 
+const Button = styled.button`
+  font-family: inherit;
+  font-size: inherit;
+  padding: 5px 10px 5px 10px;
+  background-color: black;
+  color: white;
+  border: 1px solid white;
+
+  :active {
+    background-color: #1a1a1a;
+    border: 1px solid #e7e7e7;
+  }
+
+  :hover {
+    background-color: #2a2a2a;
+    border: 1px solid #d7d7d7;
+  }
+`;
+
+const Fieldset = styled.fieldset`
+  border: none;
+  padding-left: 0px;
+  margin-left: 0px;
+`;
+
 class BinderForm extends React.Component<BinderFormProps> {
   render() {
     const { onSubmit } = this.props;
@@ -81,36 +107,10 @@ class BinderForm extends React.Component<BinderFormProps> {
       <React.Fragment>
         <form onSubmit={onSubmit} className="form">
           {this.props.children}
-          <fieldset className="binder">
-            <button type="submit">Build and Connect</button>
-          </fieldset>
+          <Fieldset>
+            <Button type="submit">Build and Connect</Button>
+          </Fieldset>
         </form>
-        <style jsx>{`
-          button {
-            font-family: inherit;
-            font-size: inherit;
-            padding: 5px 10px 5px 10px;
-            background-color: black;
-            color: white;
-            border: 1px solid white;
-          }
-
-          button:active {
-            background-color: #1a1a1a;
-            border: 1px solid #e7e7e7;
-          }
-
-          button:hover {
-            background-color: #2a2a2a;
-            border: 1px solid #d7d7d7;
-          }
-
-          :global(fieldset.binder) {
-            border: none;
-            padding-left: 0px;
-            margin-left: 0px;
-          }
-        `}</style>
       </React.Fragment>
     );
   }
@@ -123,90 +123,86 @@ type BinderLogsProps = {
   }>;
 };
 
+const Logs = styled.div`
+  margin: 5px 0px 5px 0px;
+`;
+
+const Log = styled.span`
+  padding: 0 15px 0 0px;
+  margin: 0;
+  min-height: 16px;
+  display: block;
+
+  :last-child {
+    background-color: #292929;
+  }
+`;
+
+const Phase = styled.span`
+  display: inline-block;
+  min-width: 80px;
+  padding-right: 10px;
+  text-decoration: none;
+  color: #888;
+`;
+
+const Sidebar = styled.span`
+  display: inline-block;
+  text-align: left;
+  min-width: 20px;
+  text-decoration: none;
+  color: #666;
+
+  ::before {
+    content: counter(line-numbering);
+    counter-increment: line-numbering;
+    padding-right: 1em;
+  }
+`;
+
 class BinderLogs extends React.Component<BinderLogsProps> {
   render() {
     const { logs } = this.props;
     return (
-      <div className="logs">
+      <Logs>
         {logs.length > 0
           ? logs.map((log, index) => {
               return (
-                <span className="log" key={index}>
-                  <span className="sidebar" />
-                  <span className="phase">{log.phase}</span>
+                <Log key={index}>
+                  <Sidebar />
+                  <Phase>{log.phase}</Phase>
                   <span className="content">
                     <span className="message">{log.message}</span>
                   </span>
-                </span>
+                </Log>
               );
             })
           : null}
-        <style jsx>{`
-          .log {
-            padding: 0 15px 0 0px;
-            margin: 0;
-            min-height: 16px;
-            display: block;
-          }
-
-          .logs {
-            margin: 5px 0px 5px 0px;
-          }
-
-          .phase {
-            display: inline-block;
-            min-width: 80px;
-            padding-right: 10px;
-            text-decoration: none;
-            color: #888;
-          }
-
-          .sidebar::before {
-            content: counter(line-numbering);
-            counter-increment: line-numbering;
-            padding-right: 1em;
-          }
-
-          .sidebar {
-            display: inline-block;
-            text-align: left;
-            min-width: 20px;
-            text-decoration: none;
-            color: #666;
-          }
-
-          .log:last-child {
-            background-color: #292929;
-          }
-        `}</style>
-      </div>
+      </Logs>
     );
   }
 }
 
+const ConsoleDiv = styled.div`
+  & .binder-console {
+    clear: left;
+    min-height: 42px;
+    padding: 15px 0px 20px 25px;
+    color: #f1f1f1;
+    font-family: Monaco, monospace;
+    font-size: 12px;
+    line-height: 19px;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    background-color: #1a1a1a;
+    counter-reset: line-numbering;
+    margin-top: 0;
+  }
+`;
+
 export class Console extends React.Component {
   render() {
-    return (
-      <div className="console">
-        {this.props.children}
-        <style jsx>{`
-          .binder-console {
-            clear: left;
-            min-height: 42px;
-            padding: 15px 0px 20px 25px;
-            color: #f1f1f1;
-            font-family: Monaco, monospace;
-            font-size: 12px;
-            line-height: 19px;
-            white-space: pre-wrap;
-            word-wrap: break-word;
-            background-color: #1a1a1a;
-            counter-reset: line-numbering;
-            margin-top: 0;
-          }
-        `}</style>
-      </div>
-    );
+    return <ConsoleDiv className="console">{this.props.children}</ConsoleDiv>;
   }
 }
 
