dfbaee46821a8b3f70c1e3062c23e131eddbef72
Kyle Kelley <rgbkrk@gmail.com>
2018-12-17 13:23:46 -0800

    convert Cells to styled components

--- packages/presentational-components/src/components/cells.tsx
+++ packages/presentational-components/src/components/cells.tsx
@@ -1,31 +1,17 @@
 import * as React from "react";
+import styled from "styled-components";
 
-interface CellsProps {
-  children: React.ReactNode;
-}
+export const Cells = styled.div`
+  & > * {
+    margin: 20px 0;
+  }
 
-export const Cells = (props: CellsProps) => {
-  return (
-    <React.Fragment>
-      <div className="cells">{props.children}</div>
-      <style jsx>{`
-        .cells > :global(*) {
-          margin: 20px 0;
-        }
+  & {
+    font-family: "Source Sans Pro", Helvetica Neue, Helvetica, sans-serif;
+    font-size: 16px;
+    background-color: var(--theme-app-bg);
+    color: var(--theme-app-fg);
 
-        .cells {
-          font-family: "Source Sans Pro", Helvetica Neue, Helvetica, sans-serif;
-          font-size: 16px;
-          background-color: var(--theme-app-bg);
-          color: var(--theme-app-fg);
-
-          padding-bottom: 10px;
-        }
-      `}</style>
-    </React.Fragment>
-  );
-};
-
-Cells.defaultProps = {
-  children: []
-};
+    padding-bottom: 10px;
+  }
+`;
