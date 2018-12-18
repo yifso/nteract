5ff4395f49e6768cdb304947a9c680bb66a26d00
Kyle Kelley <rgbkrk@gmail.com>
2018-12-18 13:08:19 -0800

    switch editor to styled-components

--- packages/editor/src/styled/codemirror.ts
+++ packages/editor/src/styled/codemirror.ts
@@ -1,17 +1,23 @@
-import css from "styled-jsx/css";
+import styled from "styled-components";
 
-export default css`
-  /* completions styles */
-  :global(.CodeMirror) {
+import codeMirrorCSS from "../vendored/codemirror";
+import showHintCSS from "../vendored/show-hint";
+
+export const CodeMirrorContainer = styled.div`
+  ${codeMirrorCSS}
+  ${showHintCSS}
+
+    /* completions styles */
+    .CodeMirror {
     height: 100%;
   }
 
-  :global(.CodeMirror-hint) {
+  .CodeMirror-hint {
     padding-left: 0;
     border-bottom: none;
   }
 
-  :global(.completion-type) {
+  .completion-type {
     background: transparent;
     border: transparent 1px solid;
     width: 17px;
@@ -23,59 +29,59 @@ export default css`
     top: 18px;
   }
 
-  :global(.completion-type:before) {
+  .completion-type:before {
     content: "?";
     bottom: 1px;
     left: 4px;
     position: relative;
   }
   /* color and content for each type of completion */
-  :global(.completion-type-keyword:before) {
+  .completion-type-keyword:before {
     content: "K";
   }
-  :global(.completion-type-keyword) {
+  .completion-type-keyword {
     background-color: darkred;
   }
 
-  :global(.completion-type-class:before) {
+  .completion-type-class:before {
     content: "C";
   }
-  :global(.completion-type-class) {
+  .completion-type-class {
     background-color: blueviolet;
   }
 
-  :global(.completion-type-module:before) {
+  .completion-type-module:before {
     content: "M";
   }
-  :global(.completion-type-module) {
+  .completion-type-module {
     background-color: chocolate;
   }
 
-  :global(.completion-type-statement:before) {
+  .completion-type-statement:before {
     content: "S";
   }
-  :global(.completion-type-statement) {
+  .completion-type-statement {
     background-color: forestgreen;
   }
 
-  :global(.completion-type-function:before) {
+  .completion-type-function:before {
     content: "ƒ";
   }
-  :global(.completion-type-function) {
+  .completion-type-function {
     background-color: yellowgreen;
   }
 
-  :global(.completion-type-instance:before) {
+  .completion-type-instance:before {
     content: "I";
   }
-  :global(.completion-type-instance) {
+  .completion-type-instance {
     background-color: teal;
   }
 
-  :global(.completion-type-null:before) {
+  .completion-type-null:before {
     content: "ø";
   }
-  :global(.completion-type-null) {
+  .completion-type-null {
     background-color: black;
   }
 
@@ -85,7 +91,7 @@ export default css`
     Codemirror
  */
 
-  :global(.CodeMirror) {
+  .CodeMirror {
     font-family: "Source Code Pro";
     font-size: 14px;
     line-height: 20px;
@@ -95,99 +101,99 @@ export default css`
     background: none;
   }
 
-  :global(.CodeMirror-cursor) {
+  .CodeMirror-cursor {
     border-left-width: 1px;
     border-left-style: solid;
     border-left-color: var(--cm-color, black);
   }
 
-  :global(.CodeMirror-scroll) {
+  .CodeMirror-scroll {
     overflow-x: auto !important;
     overflow-y: hidden !important;
     width: 100%;
   }
 
-  :global(.CodeMirror-lines) {
+  .CodeMirror-lines {
     padding: 0.4em;
   }
 
-  :global(.CodeMirror-linenumber) {
+  .CodeMirror-linenumber {
     padding: 0 8px 0 4px;
   }
 
-  :global(.CodeMirror-gutters) {
+  .CodeMirror-gutters {
     border-top-left-radius: 2px;
     border-bottom-left-radius: 2px;
   }
 
-  :global(.cm-s-composition.CodeMirror) {
+  .cm-s-composition.CodeMirror {
     font-family: "Source Code Pro", monospace;
     letter-spacing: 0.3px;
     word-spacing: 0px;
     background: var(--cm-background, #fafafa);
     color: var(--cm-color, black);
   }
-  :global(.cm-s-composition .CodeMirror-lines) {
+  .cm-s-composition .CodeMirror-lines {
     padding: 10px;
   }
-  :global(.cm-s-composition .CodeMirror-gutters) {
+  .cm-s-composition .CodeMirror-gutters {
     background-color: var(--cm-gutter-bg, white);
     padding-right: 10px;
     z-index: 3;
     border: none;
   }
 
-  :global(.cm-s-composition span.cm-comment) {
+  .cm-s-composition span.cm-comment {
     color: var(--cm-comment, #a86);
   }
-  :global(.cm-s-composition span.cm-keyword) {
+  .cm-s-composition span.cm-keyword {
     line-height: 1em;
     font-weight: bold;
     color: var(--cm-keyword, blue);
   }
-  :global(.cm-s-composition span.cm-string) {
+  .cm-s-composition span.cm-string {
     color: var(--cm-string, #a22);
   }
-  :global(.cm-s-composition span.cm-builtin) {
+  .cm-s-composition span.cm-builtin {
     line-height: 1em;
     font-weight: bold;
     color: var(--cm-builtin, #077);
   }
-  :global(.cm-s-composition span.cm-special) {
+  .cm-s-composition span.cm-special {
     line-height: 1em;
     font-weight: bold;
     color: var(--cm-special, #0aa);
   }
-  :global(.cm-s-composition span.cm-variable) {
+  .cm-s-composition span.cm-variable {
     color: var(--cm-variable, black);
   }
-  :global(.cm-s-composition span.cm-number),
-  :global(.cm-s-composition span.cm-atom) {
+  .cm-s-composition span.cm-number,
+  .cm-s-composition span.cm-atom {
     color: var(--cm-number, #3a3);
   }
-  :global(.cm-s-composition span.cm-meta) {
+  .cm-s-composition span.cm-meta {
     color: var(--cm-meta, #555);
   }
-  :global(.cm-s-composition span.cm-link) {
+  .cm-s-composition span.cm-link {
     color: var(--cm-link, #3a3);
   }
-  :global(.cm-s-composition span.cm-operator) {
+  .cm-s-composition span.cm-operator {
     color: var(--cm-operator, black);
   }
-  :global(.cm-s-composition span.cm-def) {
+  .cm-s-composition span.cm-def {
     color: var(--cm-def, black);
   }
-  :global(.cm-s-composition .CodeMirror-activeline-background) {
+  .cm-s-composition .CodeMirror-activeline-background {
     background: var(--cm-activeline-bg, #e8f2ff);
   }
-  :global(.cm-s-composition .CodeMirror-matchingbracket) {
+  .cm-s-composition .CodeMirror-matchingbracket {
     border-bottom: 1px solid var(--cm-matchingbracket-outline, grey);
     color: var(--cm-matchingbracket-color, black) !important;
   }
 
   /* Overwrite some of the hint Styling */
 
-  :global(.CodeMirror-hints) {
+  .CodeMirror-hints {
     -webkit-box-shadow: 2px 3px 5px rgba(0, 0, 0, 0.2);
     -moz-box-shadow: 2px 3px 5px rgba(0, 0, 0, 0.2);
     box-shadow: 2px 3px 5px rgba(0, 0, 0, 0.2);
@@ -204,14 +210,14 @@ export default css`
     overflow-y: auto;
   }
 
-  :global(.CodeMirror-hint) {
+  .CodeMirror-hint {
     border-radius: 0px;
     white-space: pre;
     cursor: pointer;
     color: var(--cm-hint-color, black);
   }
 
-  :global(li.CodeMirror-hint-active) {
+  li.CodeMirror-hint-active {
     background: var(--cm-hint-bg-active, #abd1ff);
     color: var(--cm-hint-color-active, black);
   }
--- packages/editor/src/vendored/codemirror.ts
+++ packages/editor/src/vendored/codemirror.ts
@@ -1,41 +1,41 @@
-import css from "styled-jsx/css";
+import { css } from "styled-components";
 
 // This file contains the base codemirror styles for including in the base editor
 export default css`
   /* BASICS */
 
-  :global(.CodeMirror) {
+  .CodeMirror {
     /* Set height, width, borders, and global font properties here */
     font-family: monospace;
-    height: 100%;
+    height: 300px;
     color: black;
     direction: ltr;
   }
 
   /* PADDING */
 
-  :global(.CodeMirror-lines) {
+  .CodeMirror-lines {
     padding: 4px 0; /* Vertical padding around content */
   }
-  :global(.CodeMirror pre) {
+  .CodeMirror pre {
     padding: 0 4px; /* Horizontal padding of content */
   }
 
-  :global(.CodeMirror-scrollbar-filler),
-  :global(.CodeMirror-gutter-filler) {
+  .CodeMirror-scrollbar-filler,
+  .CodeMirror-gutter-filler {
     background-color: white; /* The little square between H and V scrollbars */
   }
 
   /* GUTTER */
 
-  :global(.CodeMirror-gutters) {
+  .CodeMirror-gutters {
     border-right: 1px solid #ddd;
     background-color: #f7f7f7;
     white-space: nowrap;
   }
-  :global(.CodeMirror-linenumbers) {
+  .CodeMirror-linenumbers {
   }
-  :global(.CodeMirror-linenumber) {
+  .CodeMirror-linenumber {
     padding: 0 3px 0 5px;
     min-width: 20px;
     text-align: right;
@@ -43,39 +43,39 @@ export default css`
     white-space: nowrap;
   }
 
-  :global(.CodeMirror-guttermarker) {
+  .CodeMirror-guttermarker {
     color: black;
   }
-  :global(.CodeMirror-guttermarker-subtle) {
+  .CodeMirror-guttermarker-subtle {
     color: #999;
   }
 
   /* CURSOR */
 
-  :global(.CodeMirror-cursor) {
+  .CodeMirror-cursor {
     border-left: 1px solid black;
     border-right: none;
     width: 0;
   }
   /* Shown when moving in bi-directional text */
-  :global(.CodeMirror div.CodeMirror-secondarycursor) {
+  .CodeMirror div.CodeMirror-secondarycursor {
     border-left: 1px solid silver;
   }
-  :global(.cm-fat-cursor .CodeMirror-cursor) {
+  .cm-fat-cursor .CodeMirror-cursor {
     width: auto;
     border: 0 !important;
     background: #7e7;
   }
-  :global(.cm-fat-cursor div.CodeMirror-cursors) {
+  .cm-fat-cursor div.CodeMirror-cursors {
     z-index: 1;
   }
-  :global(.cm-fat-cursor-mark) {
+  .cm-fat-cursor-mark {
     background-color: rgba(20, 255, 20, 0.5);
     -webkit-animation: blink 1.06s steps(1) infinite;
     -moz-animation: blink 1.06s steps(1) infinite;
     animation: blink 1.06s steps(1) infinite;
   }
-  :global(.cm-animate-fat-cursor) {
+  .cm-animate-fat-cursor {
     width: auto;
     border: 0;
     -webkit-animation: blink 1.06s steps(1) infinite;
@@ -112,15 +112,15 @@ export default css`
   }
 
   /* Can style cursor different in overwrite (non-insert) mode */
-  :global(.CodeMirror-overwrite .CodeMirror-cursor) {
+  .CodeMirror-overwrite .CodeMirror-cursor {
   }
 
-  :global(.cm-tab) {
+  .cm-tab {
     display: inline-block;
     text-decoration: inherit;
   }
 
-  :global(.CodeMirror-rulers) {
+  .CodeMirror-rulers {
     position: absolute;
     left: 0;
     right: 0;
@@ -128,7 +128,7 @@ export default css`
     bottom: -20px;
     overflow: hidden;
   }
-  :global(.CodeMirror-ruler) {
+  .CodeMirror-ruler {
     border-left: 1px solid #ccc;
     top: 0;
     bottom: 0;
@@ -137,113 +137,113 @@ export default css`
 
   /* DEFAULT THEME */
 
-  :global(.cm-s-default .cm-header) {
+  .cm-s-default .cm-header {
     color: blue;
   }
-  :global(.cm-s-default .cm-quote) {
+  .cm-s-default .cm-quote {
     color: #090;
   }
-  :global(.cm-negative) {
+  .cm-negative {
     color: #d44;
   }
-  :global(.cm-positive) {
+  .cm-positive {
     color: #292;
   }
-  :global(.cm-header),
-  :global(.cm-strong) {
+  .cm-header,
+  .cm-strong {
     font-weight: bold;
   }
-  :global(.cm-em) {
+  .cm-em {
     font-style: italic;
   }
-  :global(.cm-link) {
+  .cm-link {
     text-decoration: underline;
   }
-  :global(.cm-strikethrough) {
+  .cm-strikethrough {
     text-decoration: line-through;
   }
 
-  :global(.cm-s-default .cm-keyword) {
+  .cm-s-default .cm-keyword {
     color: #708;
   }
-  :global(.cm-s-default .cm-atom) {
+  .cm-s-default .cm-atom {
     color: #219;
   }
-  :global(.cm-s-default .cm-number) {
+  .cm-s-default .cm-number {
     color: #164;
   }
-  :global(.cm-s-default .cm-def) {
+  .cm-s-default .cm-def {
     color: #00f;
   }
-  :global(.cm-s-default .cm-variable),
-  :global(.cm-s-default .cm-punctuation),
-  :global(.cm-s-default .cm-property),
-  :global(.cm-s-default .cm-operator) {
+  .cm-s-default .cm-variable,
+  .cm-s-default .cm-punctuation,
+  .cm-s-default .cm-property,
+  .cm-s-default .cm-operator {
   }
-  :global(.cm-s-default .cm-variable-2) {
+  .cm-s-default .cm-variable-2 {
     color: #05a;
   }
-  :global(.cm-s-default .cm-variable-3),
-  :global(.cm-s-default .cm-type) {
+  .cm-s-default .cm-variable-3,
+  .cm-s-default .cm-type {
     color: #085;
   }
-  :global(.cm-s-default .cm-comment) {
+  .cm-s-default .cm-comment {
     color: #a50;
   }
-  :global(.cm-s-default .cm-string) {
+  .cm-s-default .cm-string {
     color: #a11;
   }
-  :global(.cm-s-default .cm-string-2) {
+  .cm-s-default .cm-string-2 {
     color: #f50;
   }
-  :global(.cm-s-default .cm-meta) {
+  .cm-s-default .cm-meta {
     color: #555;
   }
-  :global(.cm-s-default .cm-qualifier) {
+  .cm-s-default .cm-qualifier {
     color: #555;
   }
-  :global(.cm-s-default .cm-builtin) {
+  .cm-s-default .cm-builtin {
     color: #30a;
   }
-  :global(.cm-s-default .cm-bracket) {
+  .cm-s-default .cm-bracket {
     color: #997;
   }
-  :global(.cm-s-default .cm-tag) {
+  .cm-s-default .cm-tag {
     color: #170;
   }
-  :global(.cm-s-default .cm-attribute) {
+  .cm-s-default .cm-attribute {
     color: #00c;
   }
-  :global(.cm-s-default .cm-hr) {
+  .cm-s-default .cm-hr {
     color: #999;
   }
-  :global(.cm-s-default .cm-link) {
+  .cm-s-default .cm-link {
     color: #00c;
   }
 
-  :global(.cm-s-default .cm-error) {
+  .cm-s-default .cm-error {
     color: #f00;
   }
-  :global(.cm-invalidchar) {
+  .cm-invalidchar {
     color: #f00;
   }
 
-  :global(.CodeMirror-composing) {
+  .CodeMirror-composing {
     border-bottom: 2px solid;
   }
 
   /* Default styles for common addons */
 
-  :global(div.CodeMirror span.CodeMirror-matchingbracket) {
+  div.CodeMirror span.CodeMirror-matchingbracket {
     color: #0b0;
   }
-  :global(div.CodeMirror span.CodeMirror-nonmatchingbracket) {
+  div.CodeMirror span.CodeMirror-nonmatchingbracket {
     color: #a22;
   }
-  :global(.CodeMirror-matchingtag) {
+  .CodeMirror-matchingtag {
     background: rgba(255, 150, 0, 0.3);
   }
-  :global(.CodeMirror-activeline-background) {
+  .CodeMirror-activeline-background {
     background: #e8f2ff;
   }
 
@@ -252,13 +252,13 @@ export default css`
   /* The rest of this file contains styles related to the mechanics of
    the editor. You probably shouldn't touch them. */
 
-  :global(.CodeMirror) {
+  .CodeMirror {
     position: relative;
     overflow: hidden;
     background: white;
   }
 
-  :global(.CodeMirror-scroll) {
+  .CodeMirror-scroll {
     overflow: scroll !important; /* Things will break if this is overridden */
     /* 30px is the magic margin used to hide the element's real scrollbars */
     /* See overflow: hidden in .CodeMirror */
@@ -269,7 +269,7 @@ export default css`
     outline: none; /* Prevent dragging from highlighting the element */
     position: relative;
   }
-  :global(.CodeMirror-sizer) {
+  .CodeMirror-sizer {
     position: relative;
     border-right: 30px solid transparent;
   }
@@ -277,78 +277,78 @@ export default css`
   /* The fake, visible scrollbars. Used to force redraw during scrolling
    before actual scrolling happens, thus preventing shaking and
    flickering artifacts. */
-  :global(.CodeMirror-vscrollbar),
-  :global(.CodeMirror-hscrollbar),
-  :global(.CodeMirror-scrollbar-filler),
-  :global(.CodeMirror-gutter-filler) {
+  .CodeMirror-vscrollbar,
+  .CodeMirror-hscrollbar,
+  .CodeMirror-scrollbar-filler,
+  .CodeMirror-gutter-filler {
     position: absolute;
     z-index: 6;
     display: none;
   }
-  :global(.CodeMirror-vscrollbar) {
+  .CodeMirror-vscrollbar {
     right: 0;
     top: 0;
     overflow-x: hidden;
     overflow-y: scroll;
   }
-  :global(.CodeMirror-hscrollbar) {
+  .CodeMirror-hscrollbar {
     bottom: 0;
     left: 0;
     overflow-y: hidden;
     overflow-x: scroll;
   }
-  :global(.CodeMirror-scrollbar-filler) {
+  .CodeMirror-scrollbar-filler {
     right: 0;
     bottom: 0;
   }
-  :global(.CodeMirror-gutter-filler) {
+  .CodeMirror-gutter-filler {
     left: 0;
     bottom: 0;
   }
 
-  :global(.CodeMirror-gutters) {
+  .CodeMirror-gutters {
     position: absolute;
     left: 0;
     top: 0;
     min-height: 100%;
     z-index: 3;
   }
-  :global(.CodeMirror-gutter) {
+  .CodeMirror-gutter {
     white-space: normal;
     height: 100%;
     display: inline-block;
     vertical-align: top;
     margin-bottom: -30px;
   }
-  :global(.CodeMirror-gutter-wrapper) {
+  .CodeMirror-gutter-wrapper {
     position: absolute;
     z-index: 4;
     background: none !important;
     border: none !important;
   }
-  :global(.CodeMirror-gutter-background) {
+  .CodeMirror-gutter-background {
     position: absolute;
     top: 0;
     bottom: 0;
     z-index: 4;
   }
-  :global(.CodeMirror-gutter-elt) {
+  .CodeMirror-gutter-elt {
     position: absolute;
     cursor: default;
     z-index: 4;
   }
-  :global(.CodeMirror-gutter-wrapper ::selection) {
+  .CodeMirror-gutter-wrapper ::selection {
     background-color: transparent;
   }
-  :global(.CodeMirror-gutter-wrapper ::-moz-selection) {
+  .CodeMirror-gutter-wrapper ::-moz-selection {
     background-color: transparent;
   }
 
-  :global(.CodeMirror-lines) {
+  .CodeMirror-lines {
     cursor: text;
     min-height: 1px; /* prevents collapsing before first draw */
   }
-  :global(.CodeMirror pre) {
+  .CodeMirror pre {
     /* Reset some styles that the rest of the page might have set */
     -moz-border-radius: 0;
     -webkit-border-radius: 0;
@@ -369,13 +369,13 @@ export default css`
     -webkit-font-variant-ligatures: contextual;
     font-variant-ligatures: contextual;
   }
-  :global(.CodeMirror-wrap pre) {
+  .CodeMirror-wrap pre {
     word-wrap: break-word;
     white-space: pre-wrap;
     word-break: normal;
   }
 
-  :global(.CodeMirror-linebackground) {
+  .CodeMirror-linebackground {
     position: absolute;
     left: 0;
     right: 0;
@@ -384,34 +384,34 @@ export default css`
     z-index: 0;
   }
 
-  :global(.CodeMirror-linewidget) {
+  .CodeMirror-linewidget {
     position: relative;
     z-index: 2;
     padding: 0.1px; /* Force widget margins to stay inside of the container */
   }
 
-  :global(.CodeMirror-widget) {
+  .CodeMirror-widget {
   }
 
-  :global(.CodeMirror-rtl pre) {
+  .CodeMirror-rtl pre {
     direction: rtl;
   }
 
-  :global(.CodeMirror-code) {
+  .CodeMirror-code {
     outline: none;
   }
 
   /* Force content-box sizing for the elements where we expect it */
-  :global(.CodeMirror-scroll),
-  :global(.CodeMirror-sizer),
-  :global(.CodeMirror-gutter),
-  :global(.CodeMirror-gutters),
-  :global(.CodeMirror-linenumber) {
+  .CodeMirror-scroll,
+  .CodeMirror-sizer,
+  .CodeMirror-gutter,
+  .CodeMirror-gutters,
+  .CodeMirror-linenumber {
     -moz-box-sizing: content-box;
     box-sizing: content-box;
   }
 
-  :global(.CodeMirror-measure) {
+  .CodeMirror-measure {
     position: absolute;
     width: 100%;
     height: 0;
@@ -419,73 +419,71 @@ export default css`
     visibility: hidden;
   }
 
-  :global(.CodeMirror-cursor) {
+  .CodeMirror-cursor {
     position: absolute;
     pointer-events: none;
   }
-  :global(.CodeMirror-measure pre) {
+  .CodeMirror-measure pre {
     position: static;
   }
 
-  :global(div.CodeMirror-cursors) {
+  div.CodeMirror-cursors {
     visibility: hidden;
     position: relative;
     z-index: 3;
   }
-  :global(div.CodeMirror-dragcursors) {
+  div.CodeMirror-dragcursors {
     visibility: visible;
   }
 
-  :global(.CodeMirror-focused div.CodeMirror-cursors) {
+  .CodeMirror-focused div.CodeMirror-cursors {
     visibility: visible;
   }
 
-  :global(.CodeMirror-selected) {
+  .CodeMirror-selected {
     background: #d9d9d9;
   }
-  :global(.CodeMirror-focused .CodeMirror-selected) {
+  .CodeMirror-focused .CodeMirror-selected {
     background: #d7d4f0;
   }
-  :global(.CodeMirror-crosshair) {
+  .CodeMirror-crosshair {
     cursor: crosshair;
   }
-  :global(.CodeMirror-line::selection),
-  :global(.CodeMirror-line > span::selection),
-  :global(.CodeMirror-line > span > span::selection) {
+  .CodeMirror-line::selection,
+  .CodeMirror-line > span::selection,
+  .CodeMirror-line > span > span::selection {
     background: #d7d4f0;
   }
-  :global(.CodeMirror-line::-moz-selection),
-  :global(.CodeMirror-line > span::-moz-selection),
-  :global(.CodeMirror-line > span > span::-moz-selection) {
+  .CodeMirror-line::-moz-selection,
+  .CodeMirror-line > span::-moz-selection,
+  .CodeMirror-line > span > span::-moz-selection {
     background: #d7d4f0;
   }
 
-  :global(.cm-searching) {
+  .cm-searching {
     background-color: #ffa;
     background-color: rgba(255, 255, 0, 0.4);
   }
 
   /* Used to force a border model for a node */
-  :global(.cm-force-border) {
+  .cm-force-border {
     padding-right: 0.1px;
   }
 
-  /**
-  style jsx doens't work well with media queries
   @media print {
+    /* Hide the cursor when printing */
     .CodeMirror div.CodeMirror-cursors {
       visibility: hidden;
     }
   }
-  **/
 
   /* See issue #2901 */
-  :global(.cm-tab-wrap-hack:after) {
+  .cm-tab-wrap-hack:after {
     content: "";
   }
 
   /* Help users use markselection to safely style text background */
-  :global(span.CodeMirror-selectedtext) {
+  span.CodeMirror-selectedtext {
     background: none;
   }
 `;
