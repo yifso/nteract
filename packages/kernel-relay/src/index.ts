2bf04323c6070443b63afe49dbb8ca6c5432fe2c
Kyle Kelley <rgbkrk@gmail.com>
2018-12-21 16:20:52 -0800

    collect messages together

--- packages/kernel-relay/src/index.ts
+++ packages/kernel-relay/src/index.ts
@@ -2,7 +2,7 @@ import { ApolloServer, gql } from "apollo-server";
 
 import { findAll, Kernel, launchKernel } from "@nteract/fs-kernels";
 
-import { JupyterMessage } from "@nteract/messaging";
+import { JupyterMessage, kernelInfoRequest } from "@nteract/messaging";
 
 import GraphQLJSON from "graphql-type-json";
 
@@ -39,9 +39,49 @@ interface StartKernel {
 
 const kernels: { [id: string]: Kernel } = {};
 
+const messages: {
+  [kernelId: string]: JupyterMessage[];
+} = {};
+
 const typeDefs = [Types, Query, Mutation];
 const resolvers = {
   JSON: GraphQLJSON,
+  Mutation: {
+    startKernel: async (parentValue: any, args: StartKernel) => {
+      const kernel = await launchKernel(args.name);
+
+      console.log("kernel launched", kernel);
+
+      // NOTE: we should generate IDs
+      // We're also setting a session ID within the enchannel-zmq setup, I wonder
+      // if we should use that
+      const id = kernel.connectionInfo.key;
+
+      messages[id] = [];
+
+      const subscription = kernel.channels.subscribe(
+        (message: JupyterMessage) => {
+          messages[id].push(message);
+          console.log(message);
+        }
+      );
+
+      // THOUGHT: Should we track our own messages sent out too?
+      //          Hmmmm
+      kernel.channels.next(kernelInfoRequest());
+
+      // NOTE: We are going to want to both:
+      //   subscription.unsubscribe()
+      //   AND
+      //   kernel.channels.complete()
+
+      kernels[kernel.connectionInfo.key] = kernel;
+      return {
+        id,
+        status: "launched"
+      };
+    }
+  },
   Query: {
     listKernelSpecs: async () => {
       const kernelspecs = await findAll();
@@ -56,24 +96,6 @@ const resolvers = {
     running: () => {
       return Object.keys(kernels).map(id => ({ id, status: "pretend" }));
     }
-  },
-  Mutation: {
-    startKernel: async (_parentValue: any, args: StartKernel) => {
-      const kernel = await launchKernel(args.name);
-
-      console.log("kernel launched", kernel);
-
-      // NOTE: we should generate IDs
-      // We're also setting a session ID within the enchannel-zmq setup, I wonder
-      // if we should use that
-      const id = kernel.connectionInfo.key;
-
-      kernels[kernel.connectionInfo.key] = kernel;
-      return {
-        id,
-        status: "launched"
-      };
-    }
   }
 };
 
@@ -82,14 +104,10 @@ async function main() {
   // by passing type definitions (typeDefs) and the resolvers
   // responsible for fetching the data for those types.
   const server = new ApolloServer({
-    typeDefs,
-    resolvers: resolvers as any,
-    // mocks,
-    // mockEntireSchema: false,
+    introspection: true,
     // Since we're playing around, enable features for introspection and playing on our current deployment
     // If this gets used in a "real" production capacity, introspection and playground should be disabled
     // based on NODE_ENV === "production"
-    introspection: true,
     playground: {
       /*tabs: [
         {
@@ -97,7 +115,9 @@ async function main() {
           query: ``
         }
       ]*/
-    }
+    },
+    resolvers: resolvers as any,
+    typeDefs
   });
 
   // This `listen` method launches a web-server.  Existing apps
