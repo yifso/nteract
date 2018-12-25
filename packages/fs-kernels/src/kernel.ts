1b242a47cf8a0358e77623fdc987fbc79f0ad15e
Safia Abdalla <safia@safia.rocks>
2018-12-24 20:22:33 -0600

    Add initial shutdownEpic

--- packages/fs-kernels/src/kernel.ts
+++ packages/fs-kernels/src/kernel.ts
@@ -4,7 +4,27 @@
 import { ExecaChildProcess } from "execa";
 import pidusage from "pidusage";
 
-import { Channels } from "@nteract/messaging";
+import { Observable, of, merge, empty } from "rxjs";
+import {
+  filter,
+  map,
+  tap,
+  mergeMap,
+  catchError,
+  switchMap,
+  concatMap,
+  timeout,
+  first,
+  toArray
+} from "rxjs/operators";
+
+import {
+  Channels,
+  shutdownRequest,
+  childOf,
+  ofMessageType
+} from "@nteract/messaging";
+import { createKernelRef, KernelRef } from "@nteract/types";
 
 import { JupyterConnectionInfo } from "enchannel-zmq-backend";
 
@@ -49,6 +69,7 @@ interface Usage {
 }
 
 export class Kernel {
+  id: KernelRef;
   kernelSpec: KernelSpec;
   process: ExecaChildProcess;
   connectionInfo: JupyterConnectionInfo;
@@ -56,6 +77,7 @@ export class Kernel {
   channels: Channels;
 
   constructor(launchedKernel: LaunchedKernel) {
+    this.id = createKernelRef();
     this.process = launchedKernel.spawn;
     this.connectionInfo = launchedKernel.config;
     this.kernelSpec = launchedKernel.kernelSpec;
@@ -63,12 +85,72 @@ export class Kernel {
     this.channels = launchedKernel.channels;
   }
 
-  async shutdown() {
+  shutdownEpic() {
+    const request = shutdownRequest({ restart: false });
+
+    // Try to make a shutdown request
+    // If we don't get a response within X time, force a shutdown
+    // Either way do the same cleanup
+    const shutDownHandling = this.channels.pipe(
+      childOf(request),
+      ofMessageType("shutdown_reply"),
+      first(),
+      // If we got a reply, great! :)
+      map(msg => {
+        return {
+          text: msg.content,
+          id: this.id
+        };
+      }),
+      // If we don't get a response within 2s, assume failure :(
+      timeout(1000 * 2),
+      catchError(err => of({ error: err, id: this.id })),
+      mergeMap(async action => {
+        debugger;
+        // End all communication on the channels
+        this.channels.complete();
+
+        await this.shutdownProcess();
+
+        return merge(
+          // Pass on our intermediate action (whether or not kernel ACK'd shutdown request promptly)
+          of(action),
+          // Indicate overall success (channels cleaned up)
+          of({
+            id: this.id
+          }),
+          // Inform about the state
+          of({
+            status: "shutting down",
+            id: this.id
+          })
+        );
+      }),
+      catchError(err =>
+        // Catch all, in case there were other errors here
+        of({ error: err, id: this.id })
+      )
+    );
+
+    // On subscription, send the message
+    return Observable.create(observer => {
+      const subscription = shutDownHandling.subscribe(observer);
+      this.channels.next(request);
+      return subscription;
+    });
+  }
+
+  async shutdownProcess() {
     cleanup(this.connectionFile);
     if (!this.process.killed && this.process.pid) {
       process.kill(this.process.pid);
     }
     this.process.removeAllListeners();
+  }
+
+  async shutdown() {
+    const observable = this.shutdownEpic();
+    return observable.pipe(toArray()).toPromise();
   }
 
   async getUsage(): Promise<Usage> {
