f9ad6227c67c81891ce742d3034a85ef2e4aa4c4
Shelby Sturgis <ssturgis@netflix.com>
2018-12-27 15:47:24 -0800

    adding themed-logo.ts

---
+++ applications/jupyter-extension/nteract_on_jupyter/app/components/themed-logo.ts
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { WideLogo } from "@nteract/logos";
+
+type ThemedLogoProps = {
+  height: number,
+  theme: "light" | "dark"
+};
+
+const ThemedLogo = (props: ThemedLogoProps) => (
+  <WideLogo height={props.height} theme={props.theme} />
+);
+
+ThemedLogo.defaultProps = {
+  height: 20,
+  theme: "light"
+};
+
+export { ThemedLogo };
