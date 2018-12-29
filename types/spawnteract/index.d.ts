cae5962bca632dc9659d4c457e221486f0706ba3
Kyle Kelley <rgbkrk@gmail.com>
2018-12-29 07:16:24 -0800

    include connectioninfo in local spawnteract defn

--- types/spawnteract/index.d.ts
+++ types/spawnteract/index.d.ts
@@ -1,6 +1,18 @@
 declare module "spawnteract" {
   import { ChildProcess, SpawnOptions } from "child_process";
-  import { JupyterConnectionInfo } from "enchannel-zmq-backend";
+
+  export interface JupyterConnectionInfo {
+    version: number;
+    iopub_port: number;
+    shell_port: number;
+    stdin_port: number;
+    control_port: number;
+    signature_scheme: "hmac-sha256";
+    hb_port: number;
+    ip: string;
+    key: string;
+    transport: "tcp" | "ipc";
+  }
 
   // NOTE: There are other definitions, feel free to add more as we need them
   //       until we switch to @nteract/fs-kernels
