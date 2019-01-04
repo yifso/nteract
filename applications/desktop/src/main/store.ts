a4164ad735908a639f1af70b33135413a378872e
Safia Abdalla <safia@safia.rocks>
2019-01-04 12:05:25 -0600

    Fix type errors in store.ts

--- applications/desktop/src/main/store.ts
+++ applications/desktop/src/main/store.ts
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, Middleware } from "redux";
 import { electronEnhancer } from "redux-electron-store";
 import { middlewares as coreMiddlewares } from "@nteract/core";
 
 import reducers from "./reducers.js";
 
-const middlewares = [];
+const middlewares: Middleware[] = [];
 
 /* istanbul ignore if -- only used for debugging */
 if (process.env.DEBUG === "true") {
