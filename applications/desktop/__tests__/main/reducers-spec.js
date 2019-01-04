09a1cfe287c6c90ec82ea049430068f021da8a1b
Kyle Kelley <rgbkrk@gmail.com>
2019-01-04 15:05:43 -0800

    fix the tests for desktop

--- applications/desktop/__tests__/main/reducers-spec.js
+++ applications/desktop/__tests__/main/reducers-spec.js
@@ -8,27 +8,33 @@ describe("setKernelspecs", () => {
 
     const state = reducers(initialState, {
       type: "SET_KERNELSPECS",
-      kernelSpecs: {
-        python3: {
-          files: [
-            "/usr/local/share/jupyter/kernels/python3/kernel.json",
-            "/usr/local/share/jupyter/kernels/python3/logo-32x32.png",
-            "/usr/local/share/jupyter/kernels/python3/logo-64x64.png"
-          ],
-          resources_dir: "/usr/local/share/jupyter/kernels/python3",
-          spec: { language: "python", display_name: "Python 3", argv: [Object] }
-        },
-        javascript: {
-          files: [
-            "/Users/rgbkrk/Library/Jupyter/kernels/javascript/kernel.json",
-            "/Users/rgbkrk/Library/Jupyter/kernels/javascript/logo-32x32.png",
-            "/Users/rgbkrk/Library/Jupyter/kernels/javascript/logo-64x64.png"
-          ],
-          resources_dir: "/Users/rgbkrk/Library/Jupyter/kernels/javascript",
-          spec: {
-            argv: [Object],
-            display_name: "Javascript (Node.js)",
-            language: "javascript"
+      payload: {
+        kernelSpecs: {
+          python3: {
+            files: [
+              "/usr/local/share/jupyter/kernels/python3/kernel.json",
+              "/usr/local/share/jupyter/kernels/python3/logo-32x32.png",
+              "/usr/local/share/jupyter/kernels/python3/logo-64x64.png"
+            ],
+            resources_dir: "/usr/local/share/jupyter/kernels/python3",
+            spec: {
+              language: "python",
+              display_name: "Python 3",
+              argv: [Object]
+            }
+          },
+          javascript: {
+            files: [
+              "/Users/rgbkrk/Library/Jupyter/kernels/javascript/kernel.json",
+              "/Users/rgbkrk/Library/Jupyter/kernels/javascript/logo-32x32.png",
+              "/Users/rgbkrk/Library/Jupyter/kernels/javascript/logo-64x64.png"
+            ],
+            resources_dir: "/Users/rgbkrk/Library/Jupyter/kernels/javascript",
+            spec: {
+              argv: [Object],
+              display_name: "Javascript (Node.js)",
+              language: "javascript"
+            }
           }
         }
       }
