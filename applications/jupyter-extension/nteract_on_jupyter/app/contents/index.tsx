3e54c8aa559adce32c2d4f93367dd8388b4b77c6
Shelby Sturgis <ssturgis@netflix.com>
2019-01-04 19:01:39 -0800

    adding header file, moving out part of the rendering function from Contents

--- applications/jupyter-extension/nteract_on_jupyter/app/contents/index.tsx
+++ applications/jupyter-extension/nteract_on_jupyter/app/contents/index.tsx
@@ -16,7 +16,7 @@ import urljoin from "url-join";
 import { ConnectedDirectory } from "./directory";
 import { EditableTitleOverlay } from "../components/editable-title-overlay";
 import { ThemedLogo } from "../components/themed-logo";
-import { Nav, NavRow, NavSection } from "../components/nav";
+import { Nav, NavSection } from "../components/nav";
 import LastSaved from "../components/last-saved";
 import { default as File } from "./file";
 
@@ -37,6 +37,148 @@ interface IContentsProps {
 
 interface IContentsState {
   isDialogOpen: boolean;
+}
+
+const DirectoryHeader = (props: Partial<IContentsProps>) => (
+  <Nav>
+    <NavSection>
+      {props.appBase ? (
+        <a href={urljoin(props.appBase)} role="button" title="Home">
+          <ThemedLogo />
+        </a>
+      ) : null}
+    </NavSection>
+  </Nav>
+);
+
+class FileHeader extends React.PureComponent<
+  Partial<IContentsProps>,
+  IContentsState
+> {
+  constructor(props: Partial<IContentsProps>) {
+    super(props);
+
+    this.state = {
+      isDialogOpen: false
+    };
+  }
+
+  // Determine the file handler
+  getFileHandlerIcon = () => {
+    return this.props.saving ? (
+      <SavingIcon />
+    ) : this.props.error ? (
+      <ErrorIcon />
+    ) : this.props.loading ? (
+      <LoadingIcon />
+    ) : (
+      ""
+    );
+  };
+
+  // TODO: Add more acceptable file extensions
+  hasFileExtension = (fileName: string) => {
+    if (/\.ipynb/.exec(fileName) !== null) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // TODO: Add logic for acceptable file extensions
+  addFileExtension = (fileName: string) => {
+    if (!this.hasFileExtension(fileName)) {
+      return `${fileName}.ipynb`;
+    } else {
+      return fileName;
+    }
+  };
+
+  openDialog = () => this.setState({ isDialogOpen: true });
+  closeDialog = () => this.setState({ isDialogOpen: false });
+
+  // Handles onConfirm callback for EditableText component
+  confirmTitle = (value: string) => {
+    if (value !== this.props.displayName) {
+      this.props.changeContentName({
+        contentRef: this.props.contentRef,
+        filepath: `/${value ? this.addFileExtension(value) : ""}`,
+        prevFilePath: `/${this.props.displayName}`
+      });
+    }
+
+    this.setState({ isDialogOpen: false });
+  };
+  render() {
+    const themeLogoLink = urljoin(this.props.appBase, this.props.baseDir);
+    const icon = this.getFileHandlerIcon();
+
+    return (
+      <React.Fragment>
+        <Nav>
+          <NavSection>
+            <a href={themeLogoLink} role="button" title="Home">
+              <ThemedLogo />
+            </a>
+            <div>
+              <H4 onClick={this.openDialog}>{this.props.displayName}</H4>
+              <EditableTitleOverlay
+                defaultValue={this.props.displayName}
+                isOpen={this.state.isDialogOpen}
+                onCancel={this.closeDialog}
+                onSave={this.confirmTitle}
+              />
+            </div>
+          </NavSection>
+          <NavSection>
+            <span className="icon">{icon}</span>
+            <LastSaved contentRef={this.props.contentRef} />
+          </NavSection>
+        </Nav>
+        <NotebookMenu contentRef={this.props.contentRef} />
+      </React.Fragment>
+    );
+  }
+}
+
+class Contents extends React.PureComponent<IContentsProps, IContentsState> {
+  render() {
+    const appBase = this.props.appBase;
+
+    switch (this.props.contentType) {
+      case "notebook":
+      case "file":
+      case "dummy":
+        return (
+          <React.Fragment>
+            <FileHeader
+              contentRef={this.props.contentRef}
+              displayName={this.props.displayName}
+            />
+            <File contentRef={this.props.contentRef} appBase={appBase} />
+          </React.Fragment>
+        );
+      case "directory":
+        return (
+          <React.Fragment>
+            <DirectoryHeader appBase={this.props.appBase} />
+            <ConnectedDirectory
+              appBase={appBase}
+              contentRef={this.props.contentRef}
+            />
+          </React.Fragment>
+        );
+      default:
+        return (
+          <React.Fragment>
+            <DirectoryHeader appBase={this.props.appBase} />
+            <div>
+              {`content type ${this.props.contentType} not implemented`}
+            </div>
+          </React.Fragment>
+        );
+    }
+  }
 }
 
 const mapStateToProps = (
@@ -84,143 +226,6 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
   changeContentName: (payload: actions.ChangeContentName["payload"]) =>
     dispatch(actions.changeContentName(payload))
 });
-
-class Contents extends React.PureComponent<IContentsProps, IContentsState> {
-  constructor(props: IContentsProps) {
-    super(props);
-
-    this.state = {
-      isDialogOpen: false
-    };
-  }
-
-  // Determine the file handler
-  getFileHandlerIcon = () => {
-    return this.props.saving ? (
-      <SavingIcon />
-    ) : this.props.error ? (
-      <ErrorIcon />
-    ) : this.props.loading ? (
-      <LoadingIcon />
-    ) : (
-      ""
-    );
-  };
-
-  // TODO: Add more acceptable file extensions
-  hasFileExtension = (fileName: string) => {
-    if (/\.ipynb/.exec(fileName) !== null) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  // TODO: Add logic for acceptable file extensions
-  addFileExtension = (fileName: string) => {
-    if (!this.hasFileExtension(fileName)) {
-      return `${fileName}.ipynb`;
-    } else {
-      return fileName;
-    }
-  };
-
-  openDialog = () => this.setState({ isDialogOpen: true });
-
-  closeDialog = () => this.setState({ isDialogOpen: false });
-
-  // Handles onConfirm callback for EditableText component
-  confirmTitle = (value: string) => {
-    if (value !== this.props.displayName) {
-      this.props.changeContentName({
-        contentRef: this.props.contentRef,
-        filepath: `/${value ? this.addFileExtension(value) : ""}`,
-        prevFilePath: `/${this.props.displayName}`
-      });
-    }
-
-    this.setState({ isDialogOpen: false });
-  };
-
-  render() {
-    const appBase = this.props.appBase;
-    const icon = this.getFileHandlerIcon();
-
-    switch (this.props.contentType) {
-      case "notebook":
-      case "file":
-      case "dummy":
-        const themeLogoLink = urljoin(this.props.appBase, this.props.baseDir);
-
-        return (
-          <React.Fragment>
-            <Nav>
-              <NavSection>
-                <a href={themeLogoLink} role="button" title="Home">
-                  <ThemedLogo />
-                </a>
-                <div>
-                  <H4 onClick={this.openDialog}>{this.props.displayName}</H4>
-                  <EditableTitleOverlay
-                    defaultValue={this.props.displayName}
-                    isOpen={this.state.isDialogOpen}
-                    onCancel={this.closeDialog}
-                    onSave={this.confirmTitle}
-                  />
-                </div>
-              </NavSection>
-              <NavSection>
-                <span className="icon">{icon}</span>
-                <LastSaved contentRef={this.props.contentRef} />
-              </NavSection>
-            </Nav>
-            <NotebookMenu contentRef={this.props.contentRef} />
-            <File contentRef={this.props.contentRef} appBase={appBase} />;
-          </React.Fragment>
-        );
-      case "directory":
-        return (
-          <React.Fragment>
-            <Nav>
-              <NavSection>
-                <a
-                  href={urljoin(this.props.appBase)}
-                  title="Home"
-                  role="button"
-                >
-                  <ThemedLogo />
-                </a>
-                <span>{this.props.displayName}</span>
-              </NavSection>
-            </Nav>
-            <ConnectedDirectory
-              contentRef={this.props.contentRef}
-              appBase={appBase}
-            />
-          </React.Fragment>
-        );
-      default:
-        return (
-          <React.Fragment>
-            <Nav>
-              <NavSection>
-                <a
-                  href={urljoin(this.props.appBase)}
-                  role="button"
-                  title="Home"
-                >
-                  <ThemedLogo />
-                </a>
-              </NavSection>
-            </Nav>
-            <div>
-              {`content type ${this.props.contentType} not implemented`}
-            </div>
-          </React.Fragment>
-        );
-    }
-  }
-}
 
 export default connect(
   mapStateToProps,
