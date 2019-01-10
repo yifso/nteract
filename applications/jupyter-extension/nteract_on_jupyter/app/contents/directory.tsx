8e0b324d6feef2696446fc6b42d24536a99ca5b3
Safia Abdalla <safia@safia.rocks>
2019-01-10 14:36:02 -0600

    Migrate to makeMapStateToProps for redux

--- applications/jupyter-extension/nteract_on_jupyter/app/contents/directory.tsx
+++ applications/jupyter-extension/nteract_on_jupyter/app/contents/directory.tsx
@@ -108,59 +108,67 @@ export class DirectoryApp extends React.PureComponent<DirectoryProps> {
   }
 }
 
-const mapStateToDirectoryProps = (
-  state: AppState,
-  ownProps: { contentRef: ContentRef; appBase: string }
-): DirectoryProps => {
-  const { contentRef, appBase } = ownProps;
-  const content = selectors.content(state, ownProps);
-  const contents: LightDirectoryEntry[] = [];
-  const host = selectors.currentHost(state);
-
-  if (host.type !== "jupyter") {
-    throw new Error("This component only works with jupyter servers");
-  }
-
-  if (!content || content.type !== "directory") {
-    throw new Error(
-      "The directory component should only be used with directory contents"
-    );
-  }
-
-  content.model.items.map((entryRef: ContentRef) => {
-    const row = selectors.content(state, { contentRef: entryRef });
-    if (!row) {
-      return {
-        last_modified: new Date(),
-        name: "",
-        path: "",
-        type: ""
-      };
-    }
-
-    if (row.type !== "dummy") {
-      return null;
-    }
-
-    contents.push({
-      last_modified: row.lastSaved,
-      name: row.filepath,
-      path: row.filepath,
-      type: row.assumedType
-    });
-  });
-
-  return {
-    appBase,
-    appVersion: selectors.appVersion(state),
-    content,
-    contentRef,
-    contents,
-    host
-  };
+type InitialProps = {
+  contentRef: ContentRef;
+  appBase: string;
 };
 
-export const ConnectedDirectory = connect(mapStateToDirectoryProps)(
+const makeMapStateToDirectoryProps = (
+  initialState: AppState,
+  initialProps: InitialProps
+): ((state: AppState) => DirectoryProps) => {
+  const { contentRef, appBase } = initialProps;
+  const mapStateToDirectoryProps = (state: AppState) => {
+    const content = selectors.content(state, ownProps);
+    const contents: LightDirectoryEntry[] = [];
+    const host = selectors.currentHost(state);
+
+    if (host.type !== "jupyter") {
+      throw new Error("This component only works with jupyter servers");
+    }
+
+    if (!content || content.type !== "directory") {
+      throw new Error(
+        "The directory component should only be used with directory contents"
+      );
+    }
+
+    content.model.items.map((entryRef: ContentRef) => {
+      const row = selectors.content(state, { contentRef: entryRef });
+      if (!row) {
+        return {
+          last_modified: new Date(),
+          name: "",
+          path: "",
+          type: ""
+        };
+      }
+
+      if (row.type !== "dummy") {
+        return null;
+      }
+
+      contents.push({
+        last_modified: row.lastSaved,
+        name: row.filepath,
+        path: row.filepath,
+        type: row.assumedType
+      });
+    });
+
+    return {
+      appBase,
+      appVersion: selectors.appVersion(state),
+      content,
+      contentRef,
+      contents,
+      host
+    };
+  };
+  return mapStateToDirectoryProps;
+};
+
+export const ConnectedDirectory = connect(makeMapStateToDirectoryProps)(
   DirectoryApp
 );
 
