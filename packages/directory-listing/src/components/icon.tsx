e3e4637b94febfea4d43acbed33791638ac2ef4c
Shelby Sturgis <ssturgis@netflix.com>
2019-01-15 11:04:46 -0800

    styled-components perf fix; tslint fixes

--- packages/directory-listing/src/components/icon.tsx
+++ packages/directory-listing/src/components/icon.tsx
@@ -1,31 +1,36 @@
 import { Book, FileDirectory, FileText } from "@nteract/octicons";
 import * as React from "react";
 import styled from "styled-components";
+import { ProgressPlugin } from "webpack";
 
 interface IconProps {
   color: string;
   fileType: "unknown" | "notebook" | "directory" | "file" | "dummy";
 }
 
-const IconTD = styled.td`
+const IconTD = styled.td.attrs(props => ({
+  style: {
+    color: props.color || "#0366d6"
+  }
+}))`
   padding-right: 2px;
   padding-left: 10px;
   width: 17px;
   vertical-align: middle;
   text-align: center;
   opacity: 0.95;
-  color: ${props => props.color || "#0366d6"};
 `;
 
 IconTD.displayName = "IconTD";
 
-export class Icon extends React.Component<IconProps> {
+export class Icon extends React.PureComponent<IconProps> {
   static defaultProps: Partial<IconProps> = {
-    fileType: "file",
-    color: "#0366d6"
+    color: "#0366d6",
+    fileType: "file"
   };
 
   render() {
+    const styles = { color: this.props.color };
     let icon = <FileText />;
     switch (this.props.fileType) {
       case "notebook":
@@ -41,6 +46,6 @@ export class Icon extends React.Component<IconProps> {
         icon = <FileText />;
     }
 
-    return <IconTD style={{ color: this.props.color }}>{icon}</IconTD>;
+    return <IconTD style={styles}>{icon}</IconTD>;
   }
 }
