ea1a03ae3f979558b076ecae645536fad0cd7ba3
Safia Abdalla <safia@safia.rocks>
2019-01-16 21:34:45 -0600

    Deprecate @nteract/notebook-preview

--- packages/webpack-configurator/monorepo.js
+++ packages/webpack-configurator/monorepo.js
@@ -104,11 +104,6 @@ module.exports = {
       private: false
     },
     {
-      name: "@nteract/notebook-preview",
-      version: "11.0.0-alpha.0",
-      private: false
-    },
-    {
       name: "@nteract/notebook-render",
       version: "5.0.0-alpha.0",
       private: false
