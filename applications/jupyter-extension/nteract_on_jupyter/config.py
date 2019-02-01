13b2e29546c6ebfa2fc8364b95a291bd8e0e6a2e
Carol Willing <carolcode@willingconsulting.com>
2019-01-31 23:58:22 -0800

    apply black

--- applications/jupyter-extension/nteract_on_jupyter/config.py
+++ applications/jupyter-extension/nteract_on_jupyter/config.py
@@ -1,31 +1,23 @@
 from traitlets import HasTraits, Unicode, Bool
 from traitlets.config import Configurable
 
+
 class NteractConfig(Configurable):
     """The nteract application configuration object
     """
-    name = Unicode('nteract',
-        help='The name of nteract, which is configurable for some reason')
 
-    page_url = Unicode('/nteract',
-        help='The base URL for nteract web')
+    name = Unicode('nteract', help='The name of nteract, which is configurable for some reason')
 
-    version = Unicode('',
-        help='The version of nteract web')
+    page_url = Unicode('/nteract', help='The base URL for nteract web')
 
-    settings_dir = Unicode('',
-        help='The settings directory')
+    version = Unicode('', help='The version of nteract web')
 
-    assets_dir = Unicode('',
-        help='The assets directory')
+    settings_dir = Unicode('', help='The settings directory')
 
-    dev_mode = Bool(False,
-        help='Whether the application is in dev mode')
+    assets_dir = Unicode('', help='The assets directory')
 
-    asset_url = Unicode('',
-        config=True,
-        help='Remote URL for loading assets')
+    dev_mode = Bool(False, help='Whether the application is in dev mode')
 
-    ga_code = Unicode('',
-        config=True,
-        help="Google Analytics tracking code")
+    asset_url = Unicode('', config=True, help='Remote URL for loading assets')
+
+    ga_code = Unicode('', config=True, help="Google Analytics tracking code")
--- applications/jupyter-extension/nteract_on_jupyter/utils.py
+++ applications/jupyter-extension/nteract_on_jupyter/utils.py
@@ -1,6 +1,7 @@
 import os
 from contextlib import contextmanager
 
+
 @contextmanager
 def cmd_in_new_dir(newdir):
     cur_dir = os.getcwd()
