5ea643b59f9cece44ce3c58ca0ab780db68eca82
Shelby Sturgis <ssturgis@netflix.com>
2019-03-08 16:58:40 -0800

    adding types

--- packages/reducers/src/config.ts
+++ packages/reducers/src/config.ts
@@ -1,19 +1,22 @@
-import { Map } from "immutable";
-
+// Vendor modules
 import { MergeConfigAction, SetConfigAction } from "@nteract/actions";
 import { ConfigState } from "@nteract/types";
+import { Map } from "immutable";
 
 type ConfigAction = SetConfigAction<any> | MergeConfigAction;
 
 export function setConfigAtKey(
   state: ConfigState,
   action: SetConfigAction<any>
-) {
+): Map<string, any> {
   const { key, value } = action.payload;
   return state.set(key, value);
 }
 
-export function mergeConfig(state: ConfigState, action: MergeConfigAction) {
+export function mergeConfig(
+  state: ConfigState,
+  action: MergeConfigAction
+): Map<string, any> {
   const { config } = action.payload;
   return state.merge(config);
 }
@@ -21,7 +24,7 @@ export function mergeConfig(state: ConfigState, action: MergeConfigAction) {
 export default function handleConfig(
   state: ConfigState = Map(),
   action: ConfigAction
-) {
+): Map<string, any> {
   switch (action.type) {
     case "SET_CONFIG_AT_KEY":
       return setConfigAtKey(state, action);
