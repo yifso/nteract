ebbc2683d2a47608f301d1a4529ca21ea18f6c42
Shelby Sturgis <ssturgis@netflix.com>
2019-03-18 13:50:07 -0700

    changing state from generic map to specific map

--- packages/reducers/src/core/entities/hosts.ts
+++ packages/reducers/src/core/entities/hosts.ts
@@ -1,6 +1,7 @@
 // Vendor modules
 import * as actions from "@nteract/actions";
 import {
+  HostRecord,
   makeHostsRecord,
   makeJupyterHostRecord,
   makeLocalHostRecord
@@ -9,7 +10,10 @@ import { List, Map } from "immutable";
 import { Action, Reducer } from "redux";
 import { combineReducers } from "redux-immutable";
 
-const byRef = (state = Map(), action: Action): Map<{}, {}> => {
+const byRef = (
+  state = Map() as Map<string, HostRecord>,
+  action: Action
+): Map<string, HostRecord> => {
   let typedAction;
   switch (action.type) {
     case actions.ADD_HOST:
@@ -50,7 +54,7 @@ const refs = (state = List(), action: Action): List<string> => {
 
 export const hosts: Reducer<
   {
-    byRef: Map<{}, {}>;
+    byRef: Map<string, HostRecord>;
     refs: List<string>;
   },
   Action<any>
