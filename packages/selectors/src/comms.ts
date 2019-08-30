a2c4f2018105ff11675e3e48a60778094f9af343
Safia Abdalla <safia@microsoft.com>
2019-08-30 14:34:13 -0700

    Add more selectors for comms state

--- packages/selectors/src/comms.ts
+++ packages/selectors/src/comms.ts
@@ -17,3 +17,46 @@ export const models = createSelector(
   comms,
   comms => comms.get("models")
 );
+
+/**
+ * Returns the registered comm targets that are stored in the nteract application state.
+ */
+export const targets = createSelector(
+  comms,
+  comms => comms.get("targets")
+);
+
+/**
+ * Returns the information associated with currently registered comms.
+ */
+export const info = createSelector(
+  comms,
+  comms => comms.get("info")
+);
+
+/**
+ * Returns the model associated with a comm at a certain id.
+ *
+ * @param state The current application state
+ * @param { commId }  The commId to get info for
+ */
+export const modelById = (state: AppState, { commId }: { commId: string }) =>
+  models(state).get(commId);
+
+/**
+ * Returns the handler associated with a comm target at a certain id.
+ *
+ * @param state The current application state
+ * @param { commId }  The commId to get target for
+ */
+export const targetById = (state: AppState, { commId }: { commId: string }) =>
+  targets(state).get(commId);
+
+/**
+ * Returns the information associated with a comm registered at a certain id.
+ *
+ * @param state The current application state
+ * @param { commId }  The commId to get info for
+ */
+export const infoById = (state: AppState, { commId }: { commId: string }) =>
+  info(state).get(commId);
