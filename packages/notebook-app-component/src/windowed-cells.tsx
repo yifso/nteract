d3f22fc99ce5d1fdcd1bb984790fbda778b2f18f
Safia Abdalla <safia@microsoft.com>
2019-10-09 14:40:06 -0700

    Update WindowedCells to compute cell height

--- packages/notebook-app-component/src/windowed-cells.tsx
+++ packages/notebook-app-component/src/windowed-cells.tsx
@@ -37,62 +37,104 @@ const Cells = styled.div`
   height: 2000px;
 `;
 
+const useWindowResize = () => {
+  let [size, setSize] = React.useState([0, 0]);
+  React.useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+};
+
+export const CellsContext = React.createContext({});
+
+interface CellContainerProps {
+  children: any;
+  style: any;
+  index: number;
+}
+
+const CellContainer = (props: CellContainerProps) => {
+  const { children, style, index } = props;
+  const { setSize, windowWidth } = React.useContext(CellsContext);
+  const root = React.useRef();
+  React.useEffect(() => {
+    setSize(index, root.current.getBoundingClientRect().height);
+  }, [windowWidth]);
+  return (
+    <div
+      className="cell-container"
+      key={`cell-container-${index}`}
+      style={style}
+      ref={root}
+    >
+      {children}
+    </div>
+  );
+};
+
 export class PureWindowedCells extends React.Component<WindowedCellsProps> {
+  sizeMap = React.useRef({});
+  setSize = React.useCallback((index, size) => {
+    this.sizeMap.current = { ...this.sizeMap.current, [index]: size };
+  }, []);
+  getSize = React.useCallback(index => this.sizeMap.current[index] || 100, []);
+  windowWidth = useWindowResize()[0];
+
   render() {
-    console.log(this.props.contentRef);
     return (
-      <Cells>
-        <CellCreator
-          id={this.props.cellOrder.get(0)}
-          above
-          contentRef={this.props.contentRef}
-        />
-        <AutoSizer>
-          {({ height, width }: { height: number; width: number }) => {
-            console.log(height);
-            console.log(width);
-            return (
-              <List
-                height={height}
-                itemCount={1000}
-                itemSize={(index: number) => 100}
-                width={width}
-              >
-                {({ index, style }) => {
-                  const cellId = this.props.cellOrder.get(index);
-                  console.log(index);
-                  console.log(style);
-                  return (
-                    <div
-                      className="cell-container"
-                      key={`cell-container-${cellId}`}
-                      style={style}
-                    >
-                      <DraggableCell
-                        moveCell={this.props.moveCell}
-                        id={cellId}
-                        focusCell={this.props.focusCell}
-                        contentRef={this.props.contentRef}
-                      >
-                        <ConnectedCell
+      <CellsContext.Provider
+        value={{ setSize: this.setSize, windowWidth: this.windowWidth }}
+      >
+        <Cells>
+          <CellCreator
+            id={this.props.cellOrder.get(0)}
+            above
+            contentRef={this.props.contentRef}
+          />
+          <AutoSizer>
+            {({ height, width }: { height: number; width: number }) => {
+              return (
+                <List
+                  height={height}
+                  itemCount={this.props.cellOrder.size}
+                  itemSize={this.getSize}
+                  width={width}
+                >
+                  {({ index, style }) => {
+                    const cellId = this.props.cellOrder.get(index);
+                    return (
+                      <CellContainer index={index} style={style}>
+                        <DraggableCell
+                          moveCell={this.props.moveCell}
                           id={cellId}
+                          focusCell={this.props.focusCell}
+                          contentRef={this.props.contentRef}
+                        >
+                          <ConnectedCell
+                            id={cellId}
+                            contentRef={this.props.contentRef}
+                          />
+                        </DraggableCell>
+                        <CellCreator
+                          key={`creator-${cellId}`}
+                          id={cellId}
+                          above={false}
                           contentRef={this.props.contentRef}
                         />
-                      </DraggableCell>
-                      <CellCreator
-                        key={`creator-${cellId}`}
-                        id={cellId}
-                        above={false}
-                        contentRef={this.props.contentRef}
-                      />
-                    </div>
-                  );
-                }}
-              </List>
-            );
-          }}
-        </AutoSizer>
-      </Cells>
+                      </CellContainer>
+                    );
+                  }}
+                </List>
+              );
+            }}
+          </AutoSizer>
+        </Cells>
+      </CellsContext.Provider>
     );
   }
 }
