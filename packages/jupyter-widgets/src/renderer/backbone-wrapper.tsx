6dbc4c4b1fd6c0176d9ac2c7bc0a65953637d257
Michael Duncan <miduncan@microsoft.com>
2019-10-10 14:57:52 -0700

    Added a widget manager stub

--- packages/jupyter-widgets/src/renderer/backbone-wrapper.tsx
+++ packages/jupyter-widgets/src/renderer/backbone-wrapper.tsx
@@ -2,6 +2,8 @@ import Backbone from "backbone";
 import * as React from "react";
 
 import * as Widgets from "@jupyter-widgets/controls";
+import {WidgetManager} from "./widget-manager";
+import { WidgetView, WidgetModel } from "@jupyter-widgets/base";
 
 type Indexed = { [index: string]: any };
 
@@ -17,18 +19,24 @@ export default class BackboneWrapper extends React.Component<Props> {
     this.widgetContainerRef = React.createRef<HTMLDivElement>();
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const { model } = this.props;
     const viewName: string = model["_view_name"];
     const modelName: string = model["_model_name"];
-    if (viewName) {
-      const WidgetView = (Widgets as Indexed)[viewName];
-      const WidgetModel = (Widgets as Indexed)[modelName];
-      const widget = new WidgetView({
-        model: new WidgetModel(model),
-        el: this.widgetContainerRef.current
-      });
-      widget.render();
+    if (viewName && this.widgetContainerRef.current !== null) {
+      const manager = new WidgetManager(this.widgetContainerRef.current);
+      let newModel = await manager.new_model({
+        model_id: 'hello',
+        model_name: model._model_name,
+        model_module: model._model_module,
+        model_module_version: model._module_version,
+        view_name: model._view_name,
+        view_module: model._view_module,
+        view_module_version: model._view_module_version,        
+      }, model);
+      console.dir(newModel);
+      let newView = await manager.create_view(newModel, {});
+      newView.render();
     }
   }
 
