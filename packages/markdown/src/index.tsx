5af7e65dd6ea98eea28c87a815d075b60420bb87
Xinyan Chen <xinychen@microsoft.com>
2019-10-14 15:03:45 +0800

    Typing cleanup. Removed `any` usage.

--- packages/markdown/src/index.tsx
+++ packages/markdown/src/index.tsx
@@ -5,30 +5,33 @@ import ReactMarkdown from "react-markdown";
 
 import RemarkMathPlugin from "./remark-math";
 
-const math = (props: { value: string }): React.ReactNode => (
+const math = (props: { value: string }): React.ReactElement<unknown> => (
   <MathJax.Node>{props.value}</MathJax.Node>
 );
 
-const inlineMath = (props: { value: string }): React.ReactNode => (
+const inlineMath = (props: { value: string }): React.ReactElement<unknown> => (
   <MathJax.Node inline>{props.value}</MathJax.Node>
 );
 
-const code = (props: { language: string; value: string }): React.ReactNode => (
-  <Source language={props.language}>{props.value}</Source>
-);
+const code = (props: {
+  language: string;
+  value: string
+}): React.ReactElement<unknown> => (
+    <Source language={props.language}>{props.value}</Source>
+  );
 
 const MarkdownRender = (props: ReactMarkdown.ReactMarkdownProps) => {
-  const newProps = {
+  const newProps: ReactMarkdown.ReactMarkdownProps = {
     // https://github.com/rexxars/react-markdown#options
     ...props,
     escapeHtml: false,
-    plugins: [RemarkMathPlugin],
     renderers: {
-      math,
-      inlineMath,
       code,
+      inlineMath,
+      math,
       ...props.renderers
-    } as any
+    },
+    plugins: [RemarkMathPlugin]
   };
 
   return <ReactMarkdown {...newProps} />;
