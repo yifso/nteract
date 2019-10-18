c5d890ad3c72b23ed4f53e0b03fda0943b55437c
Alex Panov <8239356+alpaix@users.noreply.github.com>
2019-10-17 19:39:31 -0700

    Cleanup the action definitions in hosts.ts
    This PR promotes the clean concise action definition style
    to avoid code repetition as proposed in #4561.
    
    The new pattern is only applied to action definitions in `hosts.ts`.
    
    Question to reviewers:
    This file contains four bookstore actions and one host action which is not consistent. Would it make sense to split it?

--- packages/actions/src/actions/hosts.ts
+++ packages/actions/src/actions/hosts.ts
@@ -1,39 +1,34 @@
-// Vendor modules
-import { BookstoreDataModel, ContentRef } from "@nteract/types";
-
 // Local modules
 import * as actionTypes from "../actionTypes";
 
-export const publishToBookstore = (payload: {
-  contentRef: ContentRef;
-}): actionTypes.PublishToBookstore => {
+export const publishToBookstore = (
+  payload: actionTypes.PublishToBookstore["payload"]
+): actionTypes.PublishToBookstore => {
   return {
     type: actionTypes.PUBLISH_TO_BOOKSTORE,
     payload
   };
 };
 
-export const publishToBookstoreAfterSave = (payload: {
-  contentRef: ContentRef;
-  model: BookstoreDataModel;
-}): actionTypes.PublishToBookstoreAfterSave => {
+export const publishToBookstoreAfterSave = (
+  payload: actionTypes.PublishToBookstoreAfterSave["payload"]
+): actionTypes.PublishToBookstoreAfterSave => {
   return {
     type: actionTypes.PUBLISH_TO_BOOKSTORE_AFTER_SAVE,
     payload
   };
 };
 
-export const publishToBookstoreSucceeded = (payload: {
-  contentRef: ContentRef;
-}): actionTypes.PublishToBookstoreSucceeded => ({
+export const publishToBookstoreSucceeded = (
+  payload: actionTypes.PublishToBookstoreSucceeded["payload"]
+): actionTypes.PublishToBookstoreSucceeded => ({
   type: actionTypes.PUBLISH_TO_BOOKSTORE_SUCCEEDED,
   payload
 });
 
-export const publishToBookstoreFailed = (payload: {
-  contentRef: ContentRef;
-  error: Error;
-}): actionTypes.PublishToBookstoreFailed => ({
+export const publishToBookstoreFailed = (
+  payload: actionTypes.PublishToBookstoreFailed["payload"]
+): actionTypes.PublishToBookstoreFailed => ({
   type: actionTypes.PUBLISH_TO_BOOKSTORE_FAILED,
   payload
 });
