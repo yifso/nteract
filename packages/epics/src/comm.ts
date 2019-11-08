e7bfd6c667160ad441e9f8c90249f2f131d67293
Safia Abdalla <safia@microsoft.com>
2019-11-08 10:20:25 -0800

    Add initial implementation of LinkModel handling

--- packages/epics/src/comm.ts
+++ packages/epics/src/comm.ts
@@ -1,16 +1,19 @@
-import { ofMessageType } from "@nteract/messaging";
+import { ofMessageType, JupyterMessage } from "@nteract/messaging";
 import { ofType } from "redux-observable";
 import { ActionsObservable } from "redux-observable";
-import { merge } from "rxjs";
-import { map, retry, switchMap, takeUntil } from "rxjs/operators";
+import { merge, of } from "rxjs";
+import { map, filter, switchMap, takeUntil } from "rxjs/operators";
 
 import {
   commMessageAction,
   commOpenAction,
   KILL_KERNEL_SUCCESSFUL,
   LAUNCH_KERNEL_SUCCESSFUL,
-  NewKernelAction
+  NewKernelAction,
+  appendOutput
 } from "@nteract/actions";
+
+import { selectors } from "@nteract/core";
 
 /**
  * An epic that emits comm actions from the backend kernel
@@ -18,14 +21,52 @@ import {
  * @param  {redux.Store} store   the redux store
  * @return {ActionsObservable}         Comm actions
  */
-export const commListenEpic = (action$: ActionsObservable<NewKernelAction>) =>
+export const commListenEpic = (
+  action$: ActionsObservable<NewKernelAction>,
+  state$
+) =>
   action$.pipe(
     // A LAUNCH_KERNEL_SUCCESSFUL action indicates we have a new channel
     ofType(LAUNCH_KERNEL_SUCCESSFUL),
     switchMap((action: NewKernelAction) => {
       const {
-        payload: { kernel }
+        payload: { kernel, contentRef }
       } = action;
+      const model = selectors.model(state$.value, { contentRef });
+      // Listen for comm_open messages from the kernel that are associated
+      // with models that will not be rendered on the page
+      const ipywidgetsModel$ = kernel.channels.pipe(
+        ofMessageType("comm_open"),
+        filter((msg: JupyterMessage) => {
+          if (msg.content.data.state._model_name === "LinkModel") {
+            return true;
+          }
+          return false;
+        }),
+        switchMap((msg: JupyterMessage) =>
+          of(
+            commOpenAction(msg),
+            appendOutput({
+              id: model.notebook.cellOrder.first(),
+              contentRef,
+              output: {
+                output_type: "display_data",
+                data: {
+                  "application/vnd.jupyter.widget-view+json": {
+                    model_id: msg.content.comm_id,
+                    version_major: 2,
+                    version_minor: 0
+                  }
+                },
+                metadata: {},
+                transient: {}
+              }
+            })
+          )
+        ),
+        takeUntil(action$.pipe(ofType(KILL_KERNEL_SUCCESSFUL)))
+      );
+
       // Listen on the comms channel until KILL_KERNEL_SUCCESSFUL is emitted
       const commOpenAction$ = kernel.channels.pipe(
         ofMessageType("comm_open"),
@@ -39,6 +80,6 @@ export const commListenEpic = (action$: ActionsObservable<NewKernelAction>) =>
         takeUntil(action$.pipe(ofType(KILL_KERNEL_SUCCESSFUL)))
       );
 
-      return merge(commOpenAction$, commMessageAction$);
+      return merge(ipywidgetsModel$, commOpenAction$, commMessageAction$);
     })
   );
