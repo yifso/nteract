ea71df62a53616192bd873f665f55dd1ec4c44dd
Safia Abdalla <safia@safia.rocks>
2020-01-27 12:27:29 -0800

    Add tests for DraggableCell decorator

--- packages/notebook-app-component/__tests__/decorators/draggable-cell.spec.tsx
+++ packages/notebook-app-component/__tests__/decorators/draggable-cell.spec.tsx
@@ -1,8 +1,11 @@
 import { emptyMarkdownCell } from "@nteract/commutable";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import React from "react";
 
-import DraggableCell from "../../src/decorators/draggable";
+import DraggableCell, {
+  DraggableCellView,
+  isDragUpper
+} from "../../src/decorators/draggable";
 
 // Spoof DND manager for tests.
 const dragDropManager = {
@@ -23,5 +26,66 @@ describe("DraggableCell", () => {
       context: { dragDropManager }
     });
     expect(cell).not.toBeNull();
+  });
+});
+
+describe("DraggableCellView", () => {
+  test("can be rendered", () => {
+    const connectDragPreview = jest.fn(el => el);
+    const connectDragSource = jest.fn(el => el);
+    const connectDropTarget = jest.fn(el => el);
+    const focusCell = jest.fn();
+    const moveCell = jest.fn();
+    const cell = mount(
+      <DraggableCellView
+        connectDragPreview={connectDragPreview}
+        connectDragSource={connectDragSource}
+        isDragging={false}
+        isOver={false}
+        connectDropTarget={connectDropTarget}
+        focusCell={focusCell}
+        moveCell={moveCell}
+        id="cellId"
+        contentRef="contentRef"
+      >
+        <p>Children</p>
+      </DraggableCellView>
+    );
+    expect(cell).not.toBeNull();
+  });
+});
+
+describe("isDragUpper", () => {
+  it("returns true if the object is dragged upward", () => {
+    const el = {
+      getBoundingClientRect: jest.fn(() => ({
+        bottom: 20,
+        top: 2
+      }))
+    };
+    const monitor = {
+      getClientOffset: jest.fn(() => ({
+        top: 1,
+        y: 2
+      }))
+    };
+    const result = isDragUpper({}, monitor, el);
+    expect(result).toBe(true);
+  });
+  it("returns false if the object is dragged upward", () => {
+    const el = {
+      getBoundingClientRect: jest.fn(() => ({
+        bottom: 3,
+        top: 1
+      }))
+    };
+    const monitor = {
+      getClientOffset: jest.fn(() => ({
+        top: 1,
+        y: 10
+      }))
+    };
+    const result = isDragUpper({}, monitor, el);
+    expect(result).toBe(false);
   });
 });
--- packages/notebook-app-component/src/decorators/draggable/index.tsx
+++ packages/notebook-app-component/src/decorators/draggable/index.tsx
@@ -1,4 +1,4 @@
-import { ContentRef, actions } from "@nteract/core";
+import { actions, ContentRef } from "@nteract/core";
 import React from "react";
 import {
   ConnectDragPreview,
@@ -12,9 +12,9 @@ import {
   DropTargetMonitor
 } from "react-dnd";
 
-import styled, { StyledComponent } from "styled-components";
-import { Dispatch } from "redux";
 import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import styled, { StyledComponent } from "styled-components";
 
 /**
   The cell drag preview image is just a little stylized version of
@@ -110,7 +110,7 @@ const DragArea = styled.div.attrs<DragAreaProps>(props => ({
   padding: 10px;
 ` as StyledComponent<"div", any, DragAreaProps, never>; // Somehow setting the type on `attrs` isn't propagating properly;
 
-function isDragUpper(
+export function isDragUpper(
   props: Props,
   monitor: DropTargetMonitor,
   el: HTMLElement
@@ -175,7 +175,7 @@ function collectTarget(
   };
 }
 
-class DraggableCellView extends React.Component<
+export class DraggableCellView extends React.Component<
   Props & DnDSourceProps & DnDTargetProps,
   State
 > {
