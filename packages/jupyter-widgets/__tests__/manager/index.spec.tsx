5ce8097ae7c2e302fc6829bce9c445f54e1ebf6f
raman tehlan <ramantehlan@gmail.com>
2020-03-11 06:30:02 +0530

    add missing properties in Manager test

--- packages/jupyter-widgets/__tests__/manager/index.spec.tsx
+++ packages/jupyter-widgets/__tests__/manager/index.spec.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 
 import Manager from "../../src/manager/index";
+import { WidgetModel } from "@jupyter-widgets/base";
+
+let manager: WidgetModel;
+let modelById: (id: string) => Promise<WidgetModel>;
 
 describe("Manager", () => {
   it("can be rendered", () => {
-    expect(<Manager />).not.toBeNull();
+    expect(<Manager model_id="" 
+                    contentRef="" id="" 
+                    model={manager} 
+                    modelById={modelById} 
+                />).not.toBeNull();
   });
 });
