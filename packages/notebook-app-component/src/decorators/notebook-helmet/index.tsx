73784ca610ce1fec81d07ccafd328c40926d784c
Safia Abdalla <safia@microsoft.com>
2020-04-26 11:53:37 -0700

    Resolve LGTM and build issues

--- packages/notebook-app-component/src/decorators/notebook-helmet/index.tsx
+++ packages/notebook-app-component/src/decorators/notebook-helmet/index.tsx
@@ -1,6 +1,6 @@
 import * as selectors from "@nteract/selectors";
 import { AppState, ContentRef } from "@nteract/types";
-import React, { Component } from "react";
+import React from "react";
 import { Helmet } from "react-helmet";
 import { connect } from "react-redux";
 
