7a229a518d50caf9c253a1658d622cd97605ab4c
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2020-06-19 09:48:07 -0400

    Adds keyboard selection for command actions

--- packages/presentational-components/src/components/CommandPalette/CommandPalette.tsx
+++ packages/presentational-components/src/components/CommandPalette/CommandPalette.tsx
@@ -19,15 +19,76 @@ export interface Props
   onChangeFilter: (value: string) => void;
 }
 
-export class CommandPalette extends React.PureComponent<Props> {
+interface State {
+  selectedItemIndex: number;
+}
+
+export class CommandPalette extends React.PureComponent<Props, State> {
+  state: State = {
+    selectedItemIndex: 0,
+  };
+
+  items: React.RefObject<HTMLDivElement> = React.createRef();
+
+  handleKeys = (event: KeyboardEvent) => {
+    if (event.key === "ArrowDown") {
+      this.setState((previous) => {
+        const { children } = this.props;
+        const childLength = Array.isArray(children) ? children.length : 1;
+        const nextIndex =
+          previous.selectedItemIndex + 1 > childLength
+            ? 1
+            : previous.selectedItemIndex + 1;
+
+        return { selectedItemIndex: nextIndex };
+      });
+    }
+
+    if (event.key === "ArrowUp") {
+      this.setState((previous) => {
+        const { children } = this.props;
+        const childLength = Array.isArray(children) ? children.length : 1;
+        const nextIndex =
+          previous.selectedItemIndex - 1 === 0
+            ? childLength
+            : previous.selectedItemIndex - 1;
+
+        return { selectedItemIndex: nextIndex };
+      });
+    }
+  };
+
   componentDidMount() {
     const element: any = document.querySelector(".command-palette input");
+    addEventListener("keydown", this.handleKeys);
     element.focus();
+  }
+
+  componentWillUnmount() {
+    removeEventListener("keydown", this.handleKeys);
   }
 
   handleChangeFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.props.onChangeFilter(e.target.value);
   };
+
+  handleInputFocus = () => {
+    this.setState({ selectedItemIndex: 0 });
+  };
+
+  componentDidUpdate() {
+    if (this.state.selectedItemIndex !== 0) {
+      const item: any =
+        this.items.current &&
+        this.items.current.querySelector(
+          `button:nth-child(${this.state.selectedItemIndex})`
+        );
+
+      if (item) {
+        item.focus();
+      }
+    }
+  }
 
   render() {
     const { isVisible, onClose, shortCut, onChange, children } = this.props;
@@ -51,6 +112,7 @@ export class CommandPalette extends React.PureComponent<Props> {
           <div className="command-palette-input-row">
             <label htmlFor="commandFilter">Filter commands</label>
             <input
+              onFocus={}
               onChange={this.handleChangeFilter}
               type="text"
               name="commandFilter"
@@ -58,7 +120,9 @@ export class CommandPalette extends React.PureComponent<Props> {
               placeholder="Filter commands"
             />
           </div>
-          <div className="items">{children}</div>
+          <div className="items" ref={this.items}>
+            {children}
+          </div>
         </div>
       </React.Fragment>
     );
