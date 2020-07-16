642c73b19964a60ce224b64dc33ae478111bbf3f
Safia Abdalla <safia@microsoft.com>
2020-07-16 15:35:45 -0700

    Fix import of webpack merge in dev script

--- applications/desktop/webpack.dev.js
+++ applications/desktop/webpack.dev.js
@@ -1,5 +1,5 @@
 const webpack = require("webpack");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 
 const { commonMainConfig, commonRendererConfig } = require("./webpack.common");
 
